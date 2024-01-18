0fe1e89d30e20249eee704e46dc404b33100f8ec
Sameerkhan7509 <sameer.khan@accelgrowthtech.com>
2024-01-18 22:01:43 +0530

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,9 @@ import Button from "./Layouts/Button";
 
 const Home = () => {
   return (
-    <div className="relative min-h-screen  bg-[url('../public/Assets/hero.avif')] bg-cover bg-center bg-no-repeat w-full bg-black-900 text-white">
-      <div className="min-h-screen bg-black/70 flex flex-col justify-center items-center px-5 lg:px-20 md:px-20 leading-normal ">
-        <div className="text-center md:w-6/5 lg:w-4/5 xl:w-3/5 max-md:space-y-10 md:space-y-10 flex flex-col justify-center items-center">
+    <div className="relative bg-[url('../public/Assets/hero.avif')] bg-cover bg-center bg-no-repeat w-full bg-black-900 text-white">
+      <div className="Home h-fit bg-black/70 flex flex-col justify-center items-center px-5 lg:px-20 md:px-20 leading-normal">
+        <div className="Home-inner text-center md:w-6/5 lg:w-4/5 xl:w-3/5 md:space-y-10 lg:space-y-10 flex flex-col justify-center items-center py-12 md:py-32 lg:py-36 xl:py-40">
           <h1 className="text-xl  max-sm:text-4xl  md:text-7xl lg:text-7xl font-Playfair  font-normal">
             Only Quality Food
           </h1>
