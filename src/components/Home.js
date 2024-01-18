697f7336f7993afca2b8c80d0b0ba55ebc80f4e3
Sameerkhan7509 <sameer.khan@accelgrowthtech.com>
2024-01-18 22:30:31 +0530

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ const Home = () => {
   return (
     <div className="relative bg-[url('../public/Assets/hero.avif')] bg-cover bg-center bg-no-repeat w-full bg-black-900 text-white">
       <div className="Home h-fit bg-black/70 flex flex-col justify-center items-center px-5 lg:px-20 md:px-20 leading-normal">
-        <div className="Home-inner text-center md:w-6/5 lg:w-4/5 xl:w-3/5 md:space-y-10 lg:space-y-10 flex flex-col justify-center items-center py-12 md:py-32 lg:py-36 xl:py-40">
-          <h1 className="text-xl  max-sm:text-4xl  md:text-7xl lg:text-7xl font-Playfair  font-normal">
+        <div className="Home-inner text-center md:w-6/5 lg:w-4/5 xl:w-3/5 md:space-y-10 lg:space-y-10 flex flex-col justify-center items-center py-32 sm:py-60 md:py-72 lg:py-32 xl:py-52">
+          <h1 className="text-xl  max-sm:text-4xl sm:text-5xl md:text-7xl lg:text-7xl font-Playfair  font-normal">
             Only Quality Food
           </h1>
           <p className="text-[12px] md:text-[15px] lg:text-[17px] xl:text-[17px] font-Poppins font-medium opacity-80 tracking-wider md:leading-loose">
