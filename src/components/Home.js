35a46e7381bdf727568c9d0e0c14a32690467c10
Sameerkhan7509 <sameer.khan@accelgrowthtech.com>
2024-01-18 22:41:36 +0530

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,10 @@ import Button from "./Layouts/Button";
 
 const Home = () => {
   return (
-    <div className="relative bg-[url('../public/Assets/hero.avif')] bg-cover bg-center bg-no-repeat w-full bg-black-900 text-white">
-      <div className="Home h-fit bg-black/70 flex flex-col justify-center items-center px-5 lg:px-20 md:px-20 leading-normal">
-        <div className="Home-inner text-center md:w-6/5 lg:w-4/5 xl:w-3/5 md:space-y-10 lg:space-y-10 flex flex-col justify-center items-center py-32 sm:py-60 md:py-72 lg:py-32 xl:py-72">
-          <h1 className="text-xl  max-sm:text-4xl sm:text-5xl md:text-7xl lg:text-7xl font-Playfair  font-normal">
+    <div className="relative min-h-screen  bg-[url('../public/Assets/hero.avif')] bg-cover bg-center bg-no-repeat w-full bg-black-900 text-white">
+      <div className="min-h-screen bg-black/70 flex flex-col justify-center items-center px-5 lg:px-20 md:px-20 leading-normal ">
+        <div className="text-center md:w-6/5 lg:w-4/5 xl:w-3/5 max-md:space-y-10 md:space-y-10 flex flex-col justify-center items-center">
+          <h1 className="text-xl  max-sm:text-4xl  md:text-7xl lg:text-7xl font-Playfair  font-normal">
             Only Quality Food
           </h1>
           <p className="text-[12px] md:text-[15px] lg:text-[17px] xl:text-[17px] font-Poppins font-medium opacity-80 tracking-wider md:leading-loose">
